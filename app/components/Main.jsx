8333a982d77072affdbde7e9e3aa76084e011c49
Qianyi Kuang <Qianyi Kuang>
2017-05-05 22:12:30 -0400

    Update style folder

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
+var Nav = require('Nav');
 
 var Main = (props) => {
   return (
     <div>
-      <Nav/>
       <div>
+        <Nav />
         <div>
           <p>Main.jsx Rendered</p>
           {props.children}
